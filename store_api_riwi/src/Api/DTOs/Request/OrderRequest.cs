4e7be96f5bcc788a4fef7cfa622c3c037268bf0b
ralexale <ralexale@gmail.com>
2024-09-19 15:41:50 -0500

    feat: add DTOs for request and response

---
+++ store_api_riwi/src/Api/DTOs/Request/OrderRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace store_api_riwi.src.Api.DTOs.Request
+{
+    public class OrderRequest
+    {
+        public int ProductId { set; get; }
+        public int UserId { set; get; }
+    }
+}
---
+++ store_api_riwi/src/Api/DTOs/Response/UserResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace store_api_riwi.src.Api.DTOs.Response
+{
+    public class UserResponse
+    {
+        public int Id { get; set; }
+
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Lastname { get; set; }
+    }
+}
