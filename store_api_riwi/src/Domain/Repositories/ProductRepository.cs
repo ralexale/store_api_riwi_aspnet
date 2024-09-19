c63ab3c8425aef554324580702d032fdbba9c481
ralexale <ralexale@gmail.com>
2024-09-19 16:11:17 -0500

    feat: add repositories for order - user - product

---
+++ store_api_riwi/src/Domain/Repositories/ProductRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using store_api_riwi.src.Domain.Entities;
+
+namespace store_api_riwi.src.Domain.Repositories
+{
+    public class ProductRepository: IRepository<Product>
+    {
+
+        private StoreContext _context;
+
+        public ProductRepository(StoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> Get() => await _context.Products.ToListAsync();
+
+        public async Task<Product> GetById(int id) => await _context.Products.FindAsync(id);
+
+        public async Task Create(Product product) => await _context.Products.AddAsync(product);
+
+        public async Task Save() => await _context.SaveChangesAsync();
+
+        public void Update(Product product)
+        {
+            _context.Products.Attach(product);
+            _context.Products.Entry(product).State = EntityState.Modified;
+        }
+        public void Delete(Product product) => _context.Products.Remove(product);
+    }
+}
---
+++ store_api_riwi/src/Domain/Repositories/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using store_api_riwi.src.Domain.Entities;
+
+namespace store_api_riwi.src.Domain.Repositories
+{
+    public class UserRepository : IRepository<User>
+    {
+        private StoreContext _context;
+
+        public UserRepository(StoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<User>> Get() => await _context.Users.ToListAsync();
+
+        public async Task<User> GetById(int id) => await _context.Users.FindAsync(id);
+
+        public async Task Create(User user) => await _context.Users.AddAsync(user);
+
+        public async Task Save() => await _context.SaveChangesAsync();
+
+        public void Update(User user)
+        {
+            _context.Users.Attach(user);
+            _context.Users.Entry(user).State = EntityState.Modified;
+        }
+        public void Delete(User user) => _context.Users.Remove(user);
+    }
+}
